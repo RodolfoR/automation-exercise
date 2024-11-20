46b9b797caa2f2356e407709b21a53b042157689
Rodolfo Rocha Ext <rodolfo_una@hotmail.com>
2024-11-20 07:21:23 -0300

    criar arquivos de fixtures para massa de dados e uso de factory para gerar dados dinamicos com o modulo faker

---
+++ cypress/Factories/user_fac.js
@@ -0,0 +1,12 @@
+
+import { faker } from '@faker-js/faker';
+
+export default{
+    createUser: function(){
+        let dados ={
+            nome: faker.person.fullName(),
+            email: faker.internet.email()
+        }
+        return dados;
+    }
+}
--- cypress/e2e/AcessarUrl.cy.js
+++ cypress/e2e/AcessarUrl.cy.js
@@ -1,12 +1,21 @@
 /// <reference types = "Cypress" />
+import user_fac from '../Factories/user_fac.js'
+
 describe('Validação Login', () => {
-  const nome = "Dayane Teste"
-  const email = "dayaneTest2@gmail.com"
+  let newUser = user_fac.createUser();
+  // beforeEach(()=>{
+  //   cy.fixture('user_fixture').then((user)=>{
+  //     newUser = user;
+  //   })
+  // })
+
+
   it('Acessar URL com sucesso', () => {
     cy.visit('https://automationexercise.com/')
     cy.get('div[class="logo pull-left"]').should('be.visible')
     cy.title().should('be.eq','Automation Exercise')
   })
+
   it.only('Cadastrar novo usuário', () => {
     //Acessar a aplicação
     cy.visit('https://automationexercise.com/')
@@ -14,12 +23,12 @@ describe('Validação Login', () => {
     cy.title().should('be.eq','Automation Exercise')
     cy.get('a').contains('Signup / Login').click()
     //Passos, ações a serem executadas
-    cy.get('input[name = "name"]').should('be.visible').type(nome)
-    cy.get('input[data-qa="signup-email"]').should('be.visible').type(email)
+    cy.get('input[name = "name"]').should('be.visible').type(newUser.nome)
+    cy.get('input[data-qa="signup-email"]').should('be.visible').type(newUser.email)
     cy.get('button').contains('Signup').click()
     cy.get('h2').contains('Enter Account Information').should('be.visible')
     cy.get('#id_gender2').click()
-    cy.get("#email").should('have.value',email)
+    cy.get("#email").should('have.value', newUser.email)
     cy.get('#password').type('teste123')
     cy.get('#days').select('13')
     cy.get('#months').select('February')
@@ -31,13 +40,13 @@ describe('Validação Login', () => {
     cy.get("#state").type("Arizona")
     cy.get("#city").type("Phoenix")
     cy.get("#zipcode").type("85001")
-    cy.get("#mobile_number").type("3182456154624")
-    cy.get("button").contains("Create Account").click()
+    cy.get("#mobile_number").type("89719827123")
+    //cy.get("button").contains("Create Account").click()
     //Validação do Resultado Esperado
-    cy.get('h2[data-qa="account-created"]').should('be.visible')
-    cy.get('h2[data-qa="account-created"]').should('have.text',"Account Created!")
-    cy.get('[data-qa="continue-button"]').should('be.visible').click()
-    cy.get('[href="/logout"]').should('be.visible')
+    // cy.get('h2[data-qa="account-created"]').should('be.visible')
+    // cy.get('h2[data-qa="account-created"]').should('have.text',"Account Created!")
+    // cy.get('[data-qa="continue-button"]').should('be.visible').click()
+    // cy.get('[href="/logout"]').should('be.visible')
   })
 
 })
